579518fb3ff7ca33e24e3a440ca80edd18116022
invalidcode232 <invalidcode69@gmail.com>
2022-08-18 13:20:19 +0700

    refactor: remove colorized crate

--- src/utils/handle_module.rs
+++ src/utils/handle_module.rs
@@ -1,7 +1,7 @@
-use colored::Colorize;
-
 pub fn format(out: &mut String, find: &str, replace: String) {
     if out.find(find).is_some() {
-        *out = out.replace(find, &replace.blue().bold().to_string());
+        let clr_replace = format!("\x1b[94m{}\x1b[0m", replace);
+        *out = out.replace(find, &clr_replace);
+        // *out = out.replace(find, format);
     }
 }
