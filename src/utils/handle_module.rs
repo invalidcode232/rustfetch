074a3504a9c56c975d37561596d7b873f2329114
invalidcode232 <invalidcode69@gmail.com>
2022-08-19 15:31:54 +0700

    feat: add bold text

--- src/utils/handle_module.rs
+++ src/utils/handle_module.rs
@@ -1,6 +1,6 @@
 pub fn format(out: &mut String, find: &str, replace: String) {
     if out.find(find).is_some() {
-        let clr_replace = format!("\x1b[94m{}\x1b[0m", replace);
+        let clr_replace = format!("\x1b[1m\x1b[94m{}\x1b[0m", replace);
         *out = out.replace(find, &clr_replace);
         // *out = out.replace(find, format);
     }
